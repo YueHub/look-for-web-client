935144428bd0093dd1d313ddb53dcb05e78f0aa0
Yue <li_chuan_yue@foxmail.com>
2017-10-31 17:47:58 +0800

    add scroller to index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,8 +2,22 @@
   <div>
     <!-- header index -->
     <header-index :title="'主页'"></header-index>
+
     <!-- acticle list panel -->
-    <panel header="众寻列表" :list="list" :type="'5'" @on-img-error="onImgError"></panel>
+    <div class="list-scroller">
+      <scroller lock-x scrollbar-y use-pullup use-pulldown @on-pullup-loading="loadMore" @on-pulldown-loading="refresh" v-model="status" ref="scroller">
+        <div>
+          <panel v-for="i in n" :key="i.id" :header="众寻列表" :list="list" :type="'5'" @on-img-error="onImgError"></panel>
+        </div>
+        <!--pullup slot-->
+        <div slot="pullup" class="xs-plugin-pullup-container xs-plugin-pullup-up" style="position: absolute; width: 100%; height: 40px; bottom: -40px; text-align: center;">
+          <span v-show="status.pullupStatus === 'default'"></span>
+          <span class="pullup-arrow" v-show="status.pullupStatus === 'down' || status.pullupStatus === 'up'" :class="{'rotate': status.pullupStatus === 'up'}">↑</span>
+          <span v-show="status.pullupStatus === 'loading'"><spinner type="ios-small"></spinner></span>
+        </div>
+      </scroller>
+    </div>
+
     <!-- bottom tab bar -->
     <bottom-tab-bar></bottom-tab-bar>
   </div>
@@ -11,39 +25,98 @@
 
 <script scoped>
 import HeaderIndex from "@/components/common/HeaderIndex";
-import { Panel } from "vux";
+import { Scroller, Spinner, Panel } from "vux";
 import BottomTabBar from "@/components/common/BottomTabBar";
 
 export default {
   components: {
     "header-index": HeaderIndex,
-    "panel": Panel,
+    Scroller,
+    Spinner,
+    panel: Panel,
     "bottom-tab-bar": BottomTabBar
   },
-  data () {
+  data() {
     return {
-      list: [{
-        src: 'http://somedomain.somdomain/x.jpg',
-        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-        title: '标题一',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: '/infoboard',
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
+      list: [
+        {
+          src: "/static/imgs/404-img.png",
+          fallbackSrc: "/static/imgs/404-img.png",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/infoboard",
+          meta: {
+            source: "来源信息",
+            date: "时间",
+            other: "其他信息"
+          }
         }
-      }]
-    }
+      ],
+      n: 10,
+      n1: 10,
+      pullupEnabled: true,
+      status: {
+        pullupStatus: "default",
+        pulldownStatus: "default"
+      }
+    };
   },
-  mounted () {
-    // 挂载后异步调用后台接口获取 list 
+  mounted() {
+    // 挂载后异步调用后台接口获取 list
   },
   methods: {
     onImgError(item, $event) {
       $event;
       // console.log(item, $event)
+    },
+    loadMore() {
+      setTimeout(() => {
+        this.n += 10;
+        setTimeout(() => {
+          this.$refs.scroller.donePullup();
+        }, 10);
+      }, 2000);
+    },
+    refresh() {
+      setTimeout(() => {
+        this.n = 10;
+        this.$nextTick(() => {
+          setTimeout(() => {
+            this.$refs.scroller.donePulldown();
+            this.pullupEnabled && this.$refs.scroller.enablePullup();
+          }, 10);
+        });
+      }, 2000);
+    },
+    changePullupStatus(enabled) {
+      if (enabled) {
+        this.$refs.scroller.enablePullup();
+        this.pullupEnabled = true;
+      } else {
+        this.$refs.scroller.disablePullup();
+        this.pullupEnabled = false;
+      }
+    },
+    loadMore1() {
+      setTimeout(() => {
+        this.n1 += 10;
+        this.$nextTick(() => {
+          this.$refs.scroller1.donePullup();
+          if (this.n1 >= 30) {
+            this.$refs.scroller1.disablePullup();
+            console.log("No more data, Pullup disabled!");
+          }
+        });
+      }, 2000);
     }
   }
 };
 </script>
+
+<style scoped>
+.list-scroller {
+  margin-top: 55px;
+  margin-bottom: 40px;
+}
+
+</style>
